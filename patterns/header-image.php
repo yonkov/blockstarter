be84685405fdda2c9225d9f6d3bcd347f3ee934b
Atanas Yonkov <yonkov.atanas@gmail.com>
2023-10-28 18:13:26 +0300

    Add transparent header template part

--- patterns/header-image.php
+++ patterns/header-image.php
@@ -6,8 +6,8 @@
  */
 ?>
 
-<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/header-image.jpg","id":1876,"dimRatio":0,"focalPoint":{"x":0.5,"y":0.5},"minHeight":90,"minHeightUnit":"vh","isDark":false,"align":"full","className":"hero-image"} -->
-<div class="wp-block-cover alignfull is-light hero-image" style="min-height:90vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-1876" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/header-image.jpg" style="object-position:50% 50%" data-object-fit="cover" data-object-position="50% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","textColor":"footer"} -->
+<!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/header-image.jpg","id":1876,"dimRatio":0,"focalPoint":{"x":0.5,"y":0.5},"minHeight":100,"minHeightUnit":"vh","isDark":false,"align":"full","className":"hero-image"} -->
+<div class="wp-block-cover alignfull is-light hero-image" style="min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-1876" alt="" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/header-image.jpg" style="object-position:50% 50%" data-object-fit="cover" data-object-position="50% 50%"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","textColor":"footer"} -->
 <h2 class="has-text-align-center has-footer-color has-text-color">Blockstarter</h2>
 <!-- /wp:heading -->
 
